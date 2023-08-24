a6a9cbb67346af8516130da1c4b16370519dccec
Samir <118690585+sr5434@users.noreply.github.com>
2023-08-24 19:36:32 +0000

    Add code

---
+++ src/app/api/post/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest){
+    const accessoken = process.env.FB_ACCESS_TOKEN;
+    const apiRes = await fetch("https://api.quotable.io/random", { method: "GET", cache: 'no-store' });
+    const data = await apiRes.json();
+    const hashtags = function(data: any) {
+        let txt="";
+        for(let i=0; i < data.tags.length;  i++){
+            txt = txt + `#${data.tags[i]} `;
+        };
+        txt.slice(0, -1);
+        txt.replace(" ", "-");
+        return txt;
+    }
+    const post = `"${data.content} - ${data.author} ${hashtags(data)}"`;
+    await fetch(`https://graph.facebook.com/v17.0/120584134465038/feed?message=${encodeURIComponent(post)}&access_token=${accessoken}`, {
+        method: 'POST'
+    });
+    return NextResponse.json({ postData: post })
+}
